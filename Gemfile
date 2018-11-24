002a5d547386d9bf87b5ec1490f0795191eb334b
chepyego <jchepyego@gmail.com>
2018-11-25 01:05:39 +0300

    finished layout and routes

--- Gemfile
+++ Gemfile
@@ -3,8 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+
+# Bundle edge Rails inpstead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
+gem 'bootstrap-sass', '3.3.7'
+gem 'rails-controller-testing'
 # Use sqlite3 as the database for Active Record
 
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'user#new'
   root'static#home'
-  get 'static/Pages'
-  get 'static/home'
-  get 'static/help'
-  get 'static/about'
-  get 'static/contact'
+  get '/pages', to: 'static#Pages'
+  get '/help', to: 'static#help'
+  get '/about', to:'static#about'
+  get '/contact', to: 'static#contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
